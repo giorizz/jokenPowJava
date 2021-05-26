3dec22820c7b4aee386274fc64396ff0b761ba67
Giovanni Rizzardo <giovannirizzardo@MacBook-Pro-de-Giovanni.local>
2021-05-25 21:44:04 -0300

    game finished joken po

--- app/src/main/java/br/com/joknapp/MainActivity.java
+++ app/src/main/java/br/com/joknapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -16,30 +17,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void selectRock(View view){
+    public void selectRock(View view) {
         this.selectOption("Pedra");
     }
 
-    public void selectPaper(View view){
+    public void selectPaper(View view) {
         this.selectOption("Papel");
     }
 
-    public void selectScissors(View view){
+    public void selectScissors(View view) {
         this.selectOption("Tesoura");
     }
 
-    public void selectOption(String selectedUser){
+    public void selectOption(String selectedUser) {
 
         ImageView imageRes = findViewById(R.id.imageResult);
+        TextView textRes = findViewById(R.id.text_result);
 
         int num = new Random().nextInt(3);
         String[] options = {"Pedra", "Papel", "Tesoura"};
         String appOption = options[num];
 
-        switch (appOption){
+        switch (appOption) {
             case "Pedra":
                 imageRes.setImageResource(R.drawable.pedra);
                 break;
+            case "Papel":
+                imageRes.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                imageRes.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (
+                (appOption == "Tesoura" && selectedUser == "Papel") ||
+                        (appOption == "Papel" && selectedUser == "Pedra") ||
+                        (appOption == "Pedra" && selectedUser == "Tesoura")
+        ) { //App Winner
+            textRes.setText("O App Ganhou!!!");
+        } else if (
+                (selectedUser == "Tesoura" && appOption == "Papel") ||
+                (selectedUser == "Papel" && appOption == "Pedra") ||
+                (selectedUser == "Pedra" && appOption == "Tesoura")
+        ) { //Play Winner
+            textRes.setText("Parabéns, Você ganhou!!!");
+        } else { //A Tie
+            textRes.setText("Empatou!!!!!!");
         }
 
     }
